077e50b674776c356469b7720934efb4ccb222e0
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-10-11 15:13:20 +0200

    Shorten namespace structure

--- contao/dca/tl_magickimages_config.php
+++ contao/dca/tl_magickimages_config.php
@@ -32,7 +32,7 @@ $GLOBALS['TL_DCA'][$table] = [
                 'class' => 'DcGeneral\Data\SingleModelDataProvider',
             ],
         ],
-        'view'          => 'DcGeneral\Contao\View\Contao2BackendView\SingleModelView',
+        'view'          => 'DcGeneral\View\SingleModelView',
     ],
 
     // MetaPalettes
